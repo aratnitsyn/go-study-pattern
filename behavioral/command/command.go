48481b7143ae6281e217155a1b90b20241164957
Aleksandr <aratnitsyn@gmail.com>
2025-03-17 21:30:06 +0300

    Add pattern Command

---
+++ behavioral/command/command.go
@@ -0,0 +1,54 @@
+package command
+
+type Command interface {
+	Execute() string
+}
+
+type Receiver struct {
+}
+
+func (r *Receiver) ToggleOn() string {
+	return "Toggle On"
+}
+
+func (r *Receiver) ToggleOff() string {
+	return "Toggle Off"
+}
+
+type ToggleOnCommand struct {
+	receiver *Receiver
+}
+
+func (c *ToggleOnCommand) Execute() string {
+	return c.receiver.ToggleOn()
+}
+
+type ToggleOffCommand struct {
+	receiver *Receiver
+}
+
+func (c *ToggleOffCommand) Execute() string {
+	return c.receiver.ToggleOff()
+}
+
+type Invoker struct {
+	commands []Command
+}
+
+func (i *Invoker) StoreCommand(command Command) {
+	i.commands = append(i.commands, command)
+}
+
+func (i *Invoker) UnStoreCommand(command Command) {
+	if len(i.commands) != 0 {
+		i.commands = i.commands[:len(i.commands)-1]
+	}
+}
+
+func (i *Invoker) Execute() string {
+	var result string
+	for _, command := range i.commands {
+		result += command.Execute() + "\n"
+	}
+	return result
+}
